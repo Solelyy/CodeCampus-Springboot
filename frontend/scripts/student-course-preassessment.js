5acbfe3f553fe16bcd35bc21f96b8ed729b0f5e9
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2025-11-14 02:57:39 +0800

    Enhance course pre-assessment UI.

--- frontend/scripts/student-course-preassessment.js
+++ frontend/scripts/student-course-preassessment.js
@@ -34,7 +34,7 @@ function showScorePopup(score, totalQuestions) {
     const feedbackText = document.getElementById('feedback-text');
     const continueBtn = document.getElementById('continue-btn');
 
-    scoreText.textContent = `You scored ${score} out of ${totalQuestions}`;
+    scoreText.innerHTML = `You scored<br><span class="score-value">${score} out of ${totalQuestions}</span>`;
     feedbackText.textContent = (score / totalQuestions >= 0.7) ? "Great Job! You may now start the activities." : "Keep going, a little more practice is needed. You may now start the activities.";
 
     popup.classList.add('show');
